d170e7e897f70b5d09b58945090d68cb89a832f0
zhuy-nguyenz <graio911@gmail.com>
2021-06-09 11:36:58 +0700

    circle

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,10 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getArea());
+    }
+}
